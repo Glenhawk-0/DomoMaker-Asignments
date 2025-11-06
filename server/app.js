98250dbb3a3e03585775f04e941919945ce3be63
Lennon-0 <LennonEhare@Gmail.com>
2025-11-06 10:49:22 -0500

    i think i did this right?

--- server/app.js
+++ server/app.js
@@ -12,25 +12,28 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const dbURI = process.envMONGODB_URI || 'mongodb://localhost/DomoMaker';
 mongoose.connect(dbURI).catch((err) => {
-    if(err){
-        console.log('Could not connect to database');
-        throw err;
-
-    }
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
 });
 
 const app = express();
 
 app.use(helmet());
-app.use
-app.use
-app.use
-app.use
-app.use
+app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
+app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.engine
-app.set
-app.set
+app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/../views`);
 
 router(app);
 
+app.listen(port, (err) => {
+  if (err) { throw err; }
+  console.log(`Listening on port ${port}`);
+});
