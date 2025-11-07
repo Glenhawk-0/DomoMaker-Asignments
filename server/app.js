0a0ac55f16f9ed315c09fa7be37a85a1f942034d
Lennon-0 <LennonEhare@Gmail.com>
2025-11-07 17:12:35 -0500

    i hope this is done, end of DomoMakerC???

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
 const session = require('express-session');
-//DomoMakerC step 2
+// DomoMakerC step 2
 // const RedisStore = require('connect-redis').RedisStore;
 const { RedisStore } = require('connect-redis');
 const redis = require('redis');
@@ -37,42 +37,38 @@ redisClient.on('error', (err) => console.log('Redis Client Error', err));
 // i think this is wher i put it??
 // DomoMakerC step 7
 redisClient.connect().then(() => {
+  const app = express();
 
-const app = express();
+  app.use(helmet());
+  app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
+  app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+  app.use(compression());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-app.use(helmet());
-app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
-app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
-app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  app.use(session({
+    key: 'sessionid', // this is the name, by default its connect.sid
+    // domoMakerC step 8
+    store: new RedisStore({
+      client: redisClient,
+    }),
+    secret: 'Domo Arigato', // is this a domo thing?
+    resave: false,
+    saveUninitialized: false,
+  }));
 
-app.use(session({
-  key: 'sessionid', // this is the name, by default its connect.sid
-  // domoMakerC step 8
-  store: new RedisStore({
-    client: redisClient,
-  }),
-  secret: 'Domo Arigato', // is this a domo thing?
-  resave: false,
-  saveUninitialized: false,
-}));
+  app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
 
-app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
+  app.set('view engine', 'handlebars');
+  app.set('views', `${__dirname}/../views`);
 
-app.set('view engine', 'handlebars');
-app.set('views', `${__dirname}/../views`);
+  router(app);
 
-router(app);
-
-app.listen(port, (err) => {
-  if (err) { throw err; }
-  console.log(`Listening on port ${port}`);
+  app.listen(port, (err) => {
+    if (err) { throw err; }
+    console.log(`Listening on port ${port}`);
+  });
 });
-
-});
-
-
